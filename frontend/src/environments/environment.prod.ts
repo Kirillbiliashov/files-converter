5c7e6fa152a7352a268707ab598a78c287b949d4
Kirill Biliashov <81979605+Kirillbiliashov@users.noreply.github.com>
2025-04-13 12:43:58 +0300

    modified angular app config

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://files-converter.onrender.com/api',
+    googleAppId: '1008106191398-bbdjcb8o46kntkjochbbpcrj64o457lo.apps.googleusercontent.com'
+  };
